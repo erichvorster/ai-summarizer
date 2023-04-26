0e67b18fcdb3165deae527cad230c268a60c1833
erichvorster <erichv@infoslips.com>
2023-04-26 08:21:44 +0200

    added redux api call - call not working yet

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,25 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
 export const articleApi = createApi({
-    reducerPath: 'articleApi'
-})
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/"}),
+    prepareHeaders: (headers) => {
+        headers.set('X-RapidAPI-Key', rapidApiKey);
+        headers.set('X-RapidAPI-Host', 'host');
+
+        return headers;
+    },
+    endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: () => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+
+
+//Redux makes this function available to us, useLazy allows us to call this function whenever we want to make a request to the API
+export const { useLazyGetSummaryQuery } = articleApi;
